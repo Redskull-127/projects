14c8ef1e0facba5fc1482ade90130ae6df8c7d5d
Redskull-127 <meerraja17@gmail.com>
2022-03-27 14:06:23 +0530

    PageApi

--- script/indexscript.js
+++ script/indexscript.js
@@ -37,15 +37,20 @@ if (/Android|iPhone/i.test(navigator.userAgent)) {
   console.log("Not using mobile");
 }
 
-const api_url =
-  "https://newsapi.org/v2/everything?q=coding&apiKey=7ca85b76c38d41c09f5c0704620bbae8";
+let page = 0;
+
+// const api_url =
+  // "https://newsapi.org/v2/everything?q=coding&apiKey=7ca85b76c38d41c09f5c0704620bbae8";
+
+const api_url = "https://newsdata.io/api/1/news?apikey=pub_5842bb3690f382c591caee103bdc59be8d3f&country=in,us&category=technology&page="+page;
+
 let news_data = null;
 let counter = 0;
 const new_data = async () => {
   await fetch(api_url)
     .then((res) => res.json())
     .then((res) => {
-      news_data = res.articles;
+      news_data = res.results;
     })
     .catch((err) => {
       console.log(err);
@@ -59,13 +64,18 @@ const fetchNews = async () => {
     aut.textContent = "Wait";
   }
   const aut = document.getElementById("authorname");
-  const imgNews = document.getElementById("newsimg");
   aut.textContent = data[counter].title;
-  const imgData = `<img src=${data[counter].urlToImage} height="90"  />`;
+  const imgNews = document.getElementById("newsimg");
+  const imgData = `<img src=${data[counter].image_url} height="90"  />`;
   console.log(data[0].urlToImage);
   imgNews.innerHTML = imgData;
+  const des = document.getElementById("des");
+  des.textContent = data[counter].description;
   if (counter <= 20) {
     counter = counter + 1;
+  }
+  if (counter == 20) {
+    page++;
   }
   var i = 0;
   function move() {
@@ -73,7 +83,7 @@ const fetchNews = async () => {
       i = 1;
       var elem = document.getElementById("myBar");
       var width = 1;
-      var id = setInterval(frame, 45);
+      var id = setInterval(frame, 85);
       function frame() {
         if (width >= 100) {
           clearInterval(id);
@@ -89,4 +99,4 @@ const fetchNews = async () => {
 };
 
 fetchNews();
-setInterval(fetchNews, 5000);
+setInterval(fetchNews, 10000);
